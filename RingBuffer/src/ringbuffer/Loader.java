aaf2b60270dc50721c62dc3d13be193eaebe020f
tiagodavi70 <tiagodavi70@gmail.com>
2015-06-21 20:03:06 -0300

    Loader errado

--- RingBuffer/src/ringbuffer/Loader.java
+++ RingBuffer/src/ringbuffer/Loader.java
@@ -5,18 +5,33 @@ import java.io.IOException;
 public class Loader {
 
 	static FileInputStream ler;
-	static StringBuffer dados = new StringBuffer();
+	static String dados = new String();
+	static RingBufferItem item = new RingBufferItem();
 	
 	public static void carregarBase() throws IOException {
 
 		// leitura do arquivo
 		ler = new FileInputStream("C:\\Users\\TiagoDavi\\teste\\Dataset1.csv");
 		int letra;
+		int[] x = new int[8];
+		int[] y = new int[8];
+		
+		int i = 0;
+		String vet[] = new String[8];
 		while ((letra = ler.read()) != -1){
-			dados.append((char)letra);
+			dados += (char)letra;
 			if (letra == ((int) '\n') ) {
-				System.out.print("Colocar no ringbuffer " + dados);
-				dados.setLength(0);
+				i++;
+				if ( i > 3) {
+					vet = dados.split(";");
+					x[0] = Integer.parseInt(vet[2]);
+					y[0] = Integer.parseInt(vet[3]);
+					
+					item.setX(x);
+					item.setY(y);
+					DrawThread.addElemento(item);	
+				}
+				dados = "";
 			}
 		}
 		
