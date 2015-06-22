60a276f64c49bb332ea9602517cb49696328ca4c
ncarneiro <nikolas.carneiro@gmail.com>
2015-06-21 23:03:52 -0300

    Cor mapeada
    Comits fuleiros

--- RingBuffer/src/ringbuffer/MapperThread.java
+++ RingBuffer/src/ringbuffer/MapperThread.java
@@ -2,6 +2,8 @@ package ringbuffer;
 
 import java.awt.Color;
 
+import ringbuffer.RingBufferItem.TYPE;
+
 public class MapperThread implements Runnable {
 	
 	private Color[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.ORANGE, Color.YELLOW, Color.MAGENTA, Color.PINK, Color.CYAN, Color.DARK_GRAY, Color.BLACK, Color.WHITE};
@@ -18,9 +20,12 @@ public class MapperThread implements Runnable {
 	private boolean modify() {
 		rbi = ringBuffer.acess();
 		if (rbi!=null) {
-			//
+			//categoricos
+			rbi.setColor(colors[metadata.getColor().getValues().indexOf(metadata.getColorName())]);
 			
+			//continuos
 			//TODO
+			rbi.setType(TYPE.DRAWING);
 			return true;
 		} else {
 			return false;
