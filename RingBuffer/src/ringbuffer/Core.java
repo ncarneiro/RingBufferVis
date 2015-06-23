3b304453ecfddd11b5ea7f3d14e3594780c5cc01
ncarneiro <nikolas.carneiro@gmail.com>
2015-06-23 16:17:05 -0300

    mods
    hfxfhgk

--- RingBuffer/src/ringbuffer/Core.java
+++ RingBuffer/src/ringbuffer/Core.java
@@ -1,27 +1,27 @@
 package ringbuffer;
 
 public class Core {
-	
-	public static void main(String[] args) {
-		Core m = new Core();
+
+	private RingBuffer rb;
+	private Metadata metadata;
+
+	private LoaderThread loader;
+	private MapperThread mapper;
+	private DrawThread drawer;
+
+	public Core() {
+		metadata = new Metadata();
 	}
-	
+
 	public void runRingBuffer(int ringbufferSize, int numberOfItems) {
-		RingBuffer rb = new RingBuffer(1024, 5000);
-		LoaderThread loader = new LoaderThread(rb);
-		Metadata metadata = new Metadata();
-		MapperThread mapper = new MapperThread(metadata, rb);
-		DrawThread drawer = new DrawThread(rb);
-		Runnable[] runs = {loader, mapper, drawer};
-		//while (rb.isEnded()) {
-		while (true) {
-			if (rb.isEnded()) {
-				break;
-			} else {
-				for (Runnable r : runs) {
-					r.run();
-				}
-			}
+		rb = new RingBuffer(ringbufferSize, numberOfItems);
+		loader = new LoaderThread(rb);
+		mapper = new MapperThread(metadata, rb);
+		drawer = new DrawThread(rb);
+		while (!rb.isEnded()) {
+			loader.run();
+			mapper.run();
+			drawer.run();
 		}
 	}
 	
--- RingBuffer/src/testes/Testes3.java
+++ RingBuffer/src/testes/Testes3.java
@@ -8,7 +8,19 @@ public class Testes3 {
 	public static void main(String[] args) {
 		Core c = new Core();
 		Metadata metadata = new Metadata();
-		c.runRingBuffer(1024, metadata.getNumberOfItems());
+		for (int i = 0; i < 3; i++) {
+			c.runRingBuffer(1024, metadata.getNumberOfItems());
+			esperar(200000);
+		}
+		
 	}
-
+	
+	public static void esperar(long segundos) {
+		long ti = System.currentTimeMillis();
+		long tf = segundos+ti;
+		while (tf>ti) {
+			ti = System.currentTimeMillis();
+		}
+	}
+	
 }
